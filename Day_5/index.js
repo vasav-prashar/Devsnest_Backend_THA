7d1f64d0b1d65b3925d43217bcac580dab8d803f
Vasav Prashar <Vasav.420@gmail.com>
2021-09-11 23:35:45 +0530

    done

--- Day_5/index.js
+++ Day_5/index.js
@@ -1,2 +1,20 @@
 var fs=require("fs");
-fs.mkdirSync("Day_5/Day");
+fs.mkdirSync("Dayex");
+fs.writeFileSync("Dayex/hello.txt","hello");
+var test =require("./test");
+var obj =new test();
+console.log(obj.print())
+
+
+
+
+// fs.writeFileSync('test/hello.txt','Hello awesome people',(err)=> {
+//     ... if(err){
+//     ..... console.log(err)
+//     ..... }
+//     ... });
+//     fs.readFileSync("test/hello.txt", "utf-8");
+//     fs.appendFileSync("test/hello.txt", " (appended part) ");
+//     fs.renameSync("test/hello.txt", "test/hello1.txt");
+//     fs.unlinkSync("test/hello1.txt");
+//     fs.rmdirSync("test");
---
+++ Day_5/test.js
@@ -0,0 +1,13 @@
+
+class test{
+    print(){
+        console.log("IAm require inside");
+    }
+}
+
+class test2{
+    print (){
+        console.log('hey');
+    }
+}
+module.exports=test;
